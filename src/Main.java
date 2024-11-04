92bbfc221108d1047812994083e33301cce0f833
Szymon Dłuski <szymon76507@gmail.com>
2024-11-04 22:17:39 +0100

    feat add: add basic buttons and window layout

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-import java.awt.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Window w = new Window("okno gry");
-        w.setLayout(new FlowLayout());
+        Window w = new Window("game window");
         w.setVisible(true);
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,10 +1,53 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+
 public class Window extends JFrame {
     Window(String windowTitle) {
         super(windowTitle);
         setResizable(false);
         setSize(1280,1024);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        Init_start_menu();
+    }
+
+    private void Init_start_menu() {
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(7,3));
+
+        ImageIcon start_photo = new ImageIcon("start.png");
+
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+
+        p.add(new JLabel(""));
+        JButton start = new JButton("Start");
+        start.setIcon(start_photo);
+        p.add(start);
+        p.add(new JLabel(""));
+
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+
+        p.add(new JLabel(""));
+        p.add(new JButton("Level select"));
+        p.add(new JLabel(""));
+
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+
+        p.add(new JLabel(""));
+        p.add(new JButton("Exit"));
+        p.add(new JLabel(""));
+
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+        p.add(new JLabel(""));
+
+        add(p);
     }
 }
