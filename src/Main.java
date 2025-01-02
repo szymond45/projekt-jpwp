aadc918a0984090d63d14bcfb9258a844ef19841
Szymon Dłuski <szymon76507@gmail.com>
2025-01-02 10:51:19 +0100

    feat fix: change window name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Window w = new Window("game window");
+        Window w = new Window("chemlab");
         w.setVisible(true);
     }
 }
